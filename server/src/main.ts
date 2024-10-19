a959bcbc3298569c24dec48208d31b5c1a8b7fe8
AbdUlraHim <3bdulrahimelsherbiny@gmail.com>
2024-10-19 20:43:25 +0400

    setup pino for logging

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useLogger(app.get(Logger));
   await app.listen(process.env.PORT ?? 5000);
 }
 bootstrap();
