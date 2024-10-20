bf737f34f47d73c781dfcd9de7c19bdd4cfc999c
AbdUlraHim <3bdulrahimelsherbiny@gmail.com>
2024-10-21 02:55:59 +0400

    fix private route

--- client/src/routes/dashboard.tsx
+++ client/src/routes/dashboard.tsx
@@ -1,13 +1,23 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
+import { createFileRoute } from "@tanstack/react-router";
 import { useAuth } from "../auth/auth";
+import { useNavigate } from "@tanstack/react-router";
+import { useEffect } from "react";
+
+const Dashboard = () => {
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      navigate({ to: "/sign-in" });
+    }
+  }, [isAuthenticated, navigate]);
+
+  if (!isAuthenticated) return null;
+
+  return <div>Welcome to the Dashboard!</div>;
+};
 
 export const Route = createFileRoute("/dashboard")({
-  beforeLoad: () => {
-    const { isAuthenticated } = useAuth();
-
-    if (!isAuthenticated) {
-      throw redirect({ to: "/sign-in" });
-    }
-  },
-  component: () => <div>Welcome to the Dashboard!</div>,
+  component: Dashboard,
 });
